9342d1283e216135ac9f79d4d3eae760989c6fb1
Amibiplob <muhammadbiplob7@gmail.com>
2022-11-24 17:05:05 +0600

    login added

--- src/Component/Pages/Login.js
+++ src/Component/Pages/Login.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            Login
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  console.log(errors);
+
+  return (
+    <div className="hero">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+
+              <input
+                className="input input-bordered"
+                type="Email"
+                placeholder="Name"
+                {...register("email", { required: true })}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                className="input input-bordered"
+                type="Password"
+                placeholder="Password"
+                {...register("password", { required: true })}
+              />
+              <label className="label">
+                <Link href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </Link>
+              </label>
+            </div>
+
+            <div className="form-control mt-6">
+              <input type="submit" className="btn btn-primary" value="Login" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
